5416cf6cb58e51591f3f716db4dd6a256eed77b8
annguyen <annguyen@172-1-2-244.lightspeed.hstntx.sbcglobal.net>
2020-10-29 11:13:16 +0700

    uploadfile3

--- upload.php
+++ upload.php
@@ -1,6 +1,7 @@
 <?php
 $target_dir = "./";
 $target_file = $target_dir . basename($_FILES["file"]["name"]);  
+var_dump($_FILES);
  if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
     echo "The file ". $_FILES["file"]["name"]. " has been uploaded.";
   } else {
