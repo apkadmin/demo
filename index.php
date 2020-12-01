8838e6d663ea439b8ea9170b90cea434adad2992
apkadmin <nguyenvanan11061997@gmail.com>
2020-12-01 14:52:00 +0700

    update log

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
 <?php 
- echo '<?php echo file_get_contents("./index.php");?>';
+ header("Access-Control-Allow-Origin: *");
+function wh_log($log_msg)
+{
+    $log_file_data = 'log.log';
+    file_put_contents($log_file_data, $log_msg . "\n", FILE_APPEND);
+}
+
+if(isset($_GET['mess'])){
+	wh_log($_GET['mess']);
+	echo $_GET['mess'];
+}
+
  ?>
